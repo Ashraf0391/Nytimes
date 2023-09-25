781f1ab9b33d5a4f1e270d7ad95072ee419dbb04
Ashraf0391 <145919033+Ashraf0391@users.noreply.github.com>
2023-09-25 07:09:24 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "https://usnewsalert.com/graphql"
+	const endpoint = "https://todaynews7.com/graphql"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://usnewsalert.com/` + encodeURI(path as string)
+					`https://todaynews7.com/` + encodeURI(path as string)
 				}`,
 			},
 		};
